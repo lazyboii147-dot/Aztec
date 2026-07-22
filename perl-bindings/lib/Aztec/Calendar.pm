3b96e456a13ed436fcb986e76ef2ff9668b39149
CL34RBOX <lazyboii147@gmail.com>
2026-07-22 14:01:45 -0700

    Add Aztec::Calendar module with glyphs

---
+++ perl-bindings/lib/Aztec/Calendar.pm
@@ -0,0 +1,12 @@
+package Aztec::Calendar;
+use strict;
+use warnings;
+
+our @Glyph = qw(
+    Cipactli Ehecatl Calli Cuetzpalin Coatl
+    Miquiztli Mazatl Tochtli Atl Itzcuintli
+    Ozomahtli Malinalli Acatl Ocelotl Cuauhtli
+    Cozcacuauhtli Ollin Tecpatl Quiahuitl Xochitl
+);
+
+1;
