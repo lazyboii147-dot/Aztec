6abf8fe5713f8822bf8084c496e361769687c111
CL34RBOX <lazyboii147@gmail.com>
2026-07-24 09:07:53 -0700

    Update animals.rs

--- src/animals.rs
+++ src/animals.rs
@@ -1,1 +1,15 @@
 
+/// Sacred animals in Aztec mythology and cosmology.
+
+pub enum SacredAnimal {
+    Eagle,
+    Jaguar,
+    Serpent,
+    Xoloitzcuintli,
+    Hummingbird,
+}
+
+pub struct AnimalAttribute {
+    pub species: SacredAnimal,
+    pub mythological_significance: &'static str,
+}
